0b8f9ccaae3582cc3cbc3e896dc00d09544ca292
Kalen <kalenwillits@gmail.com>
2023-03-09 16:19:14 -0500

    Formatting for testing.

--- src/dice_algebra.hpp
+++ src/dice_algebra.hpp
@@ -6,34 +6,34 @@
 
 
 class DiceAlgebra {
-private:
-	static const std::string CHARACTER_SET;
-	std::string expr;
-	int result;
-	void eval_filters();
-	void eval_dice_pool();
-	void eval_parentheses();
-	void eval_multiplication();
-	void eval_division();
-	void eval_modulus();
-	void eval_addition();
-	void eval_subtraction();
-	void subsitute(const int &start, const int &end, std::string &sub, const size_t offset);
-	ExpressionValue get_lvalue(size_t &i, const char operand) const;
-	ExpressionValue get_rvalue(size_t &i, const char operand) const;
+	private:
+		static const std::string CHARACTER_SET;
+		std::string expr;
+		int result;
+		void eval_filters();
+		void eval_dice_pool();
+		void eval_parentheses();
+		void eval_multiplication();
+		void eval_division();
+		void eval_modulus();
+		void eval_addition();
+		void eval_subtraction();
+		void subsitute(const int &start, const int &end, std::string &sub, const size_t offset);
+		ExpressionValue get_lvalue(size_t &i, const char operand) const;
+		ExpressionValue get_rvalue(size_t &i, const char operand) const;
 
 
-public:
-	enum ValidationResponse: unsigned short {
-		IS_VALID,
-		BAD_CHAR, 
-		UNBALANCED, 
-		BAD_FILTER,
-		ZERO_DIVISION,
-	};
-	DiceAlgebra(const std::string &expr);
-	unsigned short validate() const;
-	void eval();
-	int get_result();
-	std::string get_expr();
+	public:
+		enum ValidationResponse: unsigned short {
+			IS_VALID,
+			BAD_CHAR, 
+			UNBALANCED, 
+			BAD_FILTER,
+			ZERO_DIVISION,
+		};
+		DiceAlgebra(const std::string &expr);
+		unsigned short validate() const;
+		void eval();
+		int get_result();
+		std::string get_expr();
 };
